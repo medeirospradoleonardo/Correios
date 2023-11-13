94b8df6e3b846232d0c5bdf2cbf5470f3dcc6166
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2023-11-13 20:08:44 -0300

    att

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const axios = require('axios')
-const convert = require("xml-js");
 
 async function request(url, options) {
   return new Promise((resolve, reject) => {
@@ -19,42 +18,56 @@ const getStringFiltered = (s) => {
 
 let rastreios = []
 
-const codigos = ['LB579818367HK', 'LB579934685HK', 'LB579752952HK', 'LB579944793HK', 'NL872055306BR']
+const codigos = ['LB581600726HK', 'NL991230875BR']
 
 const index = async () => {
   rastreios = await Promise.all(codigos.map(async (codigo) => (
-    await request(`https://www.cepcerto.com/ws/encomenda/${codigo}/`, {
+    await request(`https://www.websro.com.br/rastreamento-correios.php?P_COD_UNI=${codigo}`, {
       method: 'GET',
       headers: {
         'content-type': 'text/xml',
         'user-agent': 'Dart/2.18 (dart:io)',
       }
-    }).then((body) => {
-      const data = JSON.parse(
-        convert.xml2json(body.data, { compact: true, spaces: 2 })
-      );
+    }).then(async (body) => {
 
-      if (data.xml.row?.Erro?._text) {
+      if (body.data.split('<li>Status: <b>').length > 1) {
+        let status = body.data.split('<li>Status: <b>')[1].split('</b></li>')[0]
+        switch (status) {
+          case 'Objeto em trânsito - por favor aguarde':
+            const origem = body.data.split('<li>Origem: ')[1].split('</li>')[0]
+              .replace('Unidade de Tratamento - ', '')
+              .replace('Unidade de Distribuição - ', '')
+              .replace('Agência dos Correios - ', '')
+              .replace(/( )+/g, ' ').replace(' / ', '-').toUpperCase()
+
+            const destino = body.data.split('<li>Destino: ')[1].split('</li>')[0]
+              .replace('Unidade de Tratamento - ', '')
+              .replace('Unidade de Distribuição - ', '')
+              .replace('Agência dos Correios - ', '')
+              .replace(/( )+/g, ' ').replace(' / ', '-').toUpperCase()
+
+            status = `De ${origem} para ${destino}`
+            break;
+          case 'Fiscalização aduaneira concluída - aguardando pagamento':
+            status = 'Aguardando Pagamento'
+            break
+          default:
+            break;
+        }
         return (
           {
-            "eventos": null
+            "eventos": [
+              {
+                "descricao": status
+              }
+            ]
+
           }
         )
       } else {
-
         return (
           {
-            "eventos": data.xml.row.map((evento) => {
-              return {
-                "descricao": getStringFiltered(evento.descricao._text),
-                "unidade": evento.cidade._text ? {
-                  "endereco": {
-                    "cidade": getStringFiltered(evento.cidade._text),
-                    "uf": getStringFiltered(evento.uf._text)
-                  }
-                } : null
-              }
-            })
+            "eventos": null
           }
         )
       }
@@ -66,7 +79,6 @@ const index = async () => {
       )
     })
   )))
-
 
   console.log(rastreios[0].eventos[0])
   console.log(rastreios[1].eventos[0])
